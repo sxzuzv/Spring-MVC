1cab9f73a18b8d0923e447118b8bbe72557fb3c3
soojin <pofoxz@gmail.com>
2024-04-13 00:41:00 +0900

    Add: HTTP 요청 메시지 header의 편의 정보 조회

--- servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.request;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +17,8 @@ public class RequestHeaderServlet extends HttpServlet {
             throws ServletException, IOException {
         printStartLine(request);
         printHeaders(request);
+        printHeaderUtils(request);
+        printEtc(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -71,6 +74,75 @@ public class RequestHeaderServlet extends HttpServlet {
                 .forEachRemaining(headerName ->
                         System.out.println(headerName + " : " + headerName));
 
+        // Header의 특정 정보를 지정하여 확인할 수도 있다.
+        String host = request.getHeader("host");  // Host 정보를 확인한다.
+        System.out.println("host : " + host);
+
         System.out.println("=== Headers - end ===");
+        System.out.println();
+    }
+
+    private void printHeaderUtils(HttpServletRequest request) {
+        // HTTP 요청 메시지 Header에 있는 정보를 편리하게 조회한다.
+        System.out.println("=== Header 편의 조회 start ===");
+
+        System.out.println("[Host 편의 조회]");
+
+        // Host 정보
+        System.out.println("request.getServerName() = " + request.getServerName());
+
+        // port number
+        System.out.println("request.getServerPort() = " + request.getServerPort());
+
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        // Header의 Locale 정보를 모두 꺼내서 출력한다. (getLocales() 사용)
+        // 웹 브라우저 언어 설정에 따라 순서가 변경될 수 있다.
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        // 가장 우선 순위가 높은 언어를 출력한다. (getLocale() 사용)
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+
+        System.out.println("[cookie 편의 조회]");
+        // HTTP Header에 담기는 쿠키 정보를 조회한다.
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+            }
+        }
+        System.out.println();
+
+        System.out.println("[Content 편의 조회]");
+        // HTTP 요청 메시지의 Body에 데이터가 존재하는 경우, null 값이 아닌 데이터가 출력된다.
+        System.out.println("request.getContentType() : " + request.getContentType());
+        System.out.println("request.getContentLength() : " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() : " + request.getCharacterEncoding());
+
+        System.out.println("=== Header 편의 조회 end ===");
+        System.out.println();
+    }
+
+    private void printEtc(HttpServletRequest request) {
+        // 내부 네트워크 연결과 관련된 정보를 확인한다.
+        System.out.println("=== 기타 정보 조회 start ===");
+
+        System.out.println("[Remote 정보]");
+        // 요청에 대한 상세 정보를 확인한다.
+        System.out.println("request.getRemoteHost() : " + request.getRemoteHost());
+        System.out.println("request.getRemoteAddr() : " + request.getRemoteAddr());
+        System.out.println("request.getRemotePort() : " + request.getRemotePort());
+        System.out.println();
+
+        System.out.println("[Local 정보]");
+        // 현재 내 서버에 대한 정보를 확인한다.
+        System.out.println("request.getLocalName() : " + request.getLocalName());
+        System.out.println("request.getLocalAddr() : " + request.getLocalAddr());
+        System.out.println("request.getLocalPort() : " + request.getLocalPort());
+        System.out.println();
+
+        System.out.println("=== 기타 정보 조회 end ===");
+        System.out.println();
     }
 }
