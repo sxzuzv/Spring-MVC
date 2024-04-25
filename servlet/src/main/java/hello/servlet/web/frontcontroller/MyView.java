f79bb5a7f996995862a4a8f4aebec1cff43eaec6
soojin <pofoxz@gmail.com>
2024-04-26 00:17:42 +0900

    Refactor: FrontController 도입(v3) - MyView render()

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -18,5 +19,19 @@ public class MyView {
             throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Model에 있는 데이터를 모두 꺼낸 후, request.setAttribute()를 사용하여 key에 따른 value 값을 모두 저장한다.
+        modelToRequestAttribute(model, request);
+
+        // JSP가 forward 될 때, request.getAttribute를 통해 set 해둔 정보를 사용하게 된다.
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
