85bbcf72c799d63791c9794cf0863ae8b0d5ef62
soojin <pofoxz@gmail.com>
2024-04-26 15:12:43 +0900

    Add: FrontController 도입(v4) - ControllerV4 Interface

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+// Model 객체를 파라미터로 전달하며, 뷰의 이름만 반환한다.
+public interface ControllerV4 {
+    /**
+     * @param paramMap
+     * @param model
+     * @return viewName
+     */
+    String process(Map<String, Object> paramMap, Map<String, Object> model);
+}
