ab364fa88c0560aba598959b6bec78886cebf9d5
soojin <pofoxz@gmail.com>
2024-04-28 15:27:54 +0900

    Refactor: 뷰 리졸버 - OldController 반환 값 ModelAndView 변경

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -11,6 +11,11 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+
+        // 현 상태에서는 뷰의 논리 이름만 가지고는 실제 뷰를 찾을 수 없다.
+        // 실행 시, 컨트롤러는 정상 호출되지만 Whitelabel Error Page 오류가 발생한다.
+
+        // application.properties에 prefix, suffix 추가 시, 정상적으로 뷰가 출력된다.
+        return new ModelAndView("new-form");
     }
 }
