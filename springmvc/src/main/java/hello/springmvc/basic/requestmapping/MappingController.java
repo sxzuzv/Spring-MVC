9f401eca7fe93958607c60dbcce9f111ecd790ee
soojin <pofoxz@gmail.com>
2024-04-30 15:55:10 +0900

    Add: 요청 매핑 - method 속성으로 @RequestMapping HTTP method 지정

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestmapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,8 @@ public class MappingController {
     // 배열을 활용해 URL 다중 설정이 가능하다.
     // @RequestMapping({"/hello-basic", "/hello-go"})
 
-    @RequestMapping("/hello-basic")
+    // method 속성을 사용해 특정 HTTP method 지정이 가능하다.
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
     public String helloBasic() {
         log.info("hello-basic");
         return "OK!";   // 문자열을 HTTP Message Body에 그대로 넣는다.
