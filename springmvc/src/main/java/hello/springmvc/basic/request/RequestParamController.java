a8f6abeea910ffbf2ba2067e5d68d85711139304
soojin <pofoxz@gmail.com>
2024-04-30 18:24:49 +0900

    Add: HTTP 요청 파라미터 조회 - 쿼리 파라미터 전송 방식, HTML Form 전송 방식

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        // request.getParameter()를 사용해 요청 파라미터 정보를 가져와 저장한다.
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("OK!");
+    }
+}
