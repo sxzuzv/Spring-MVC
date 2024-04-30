6f337c08499ac4ffda4d8955928a23bbfe0c2377
soojin <pofoxz@gmail.com>
2024-04-30 15:59:11 +0900

    Add: 요청 매핑 - HTTP method 축약 애너테이션

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,30 @@ public class MappingController {
     private Logger log = LoggerFactory.getLogger(getClass());
 
     /**
-     * 기본 요청: '/hello-basic', '/hello-basic/' 모두 허용
-     * HTTP 메서드 모두 허용: GET, HEAD, POST, PUT, PATCH, DELETE
+     * 기본 요청: '/mapping-get-v1', '/mapping-get-v1/' 모두 허용
+     * @RequestMapping method 미지정: HTTP 메서드 모두 허용 (GET, HEAD, POST, PUT, PATCH, DELETE)
      */
     // 배열을 활용해 URL 다중 설정이 가능하다.
-    // @RequestMapping({"/hello-basic", "/hello-go"})
+    // @RequestMapping({"/mapping-get-v1", "/mapping-get-v1-go"})
 
     // method 속성을 사용해 특정 HTTP method 지정이 가능하다.
-    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
-    public String helloBasic() {
-        log.info("hello-basic");
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "OK!";   // 문자열을 HTTP Message Body에 그대로 넣는다.
+    }
+
+    /**
+     * 편리한 축약 애너테이션
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV2");
         return "OK!";   // 문자열을 HTTP Message Body에 그대로 넣는다.
     }
 }
