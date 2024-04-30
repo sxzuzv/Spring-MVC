88ae143020171316ae7898ed73a202546ed969b9
soojin <pofoxz@gmail.com>
2024-04-30 16:08:36 +0900

    Add: 요청 매핑 - @PathVariable 사용

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,10 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
@@ -38,5 +35,19 @@ public class MappingController {
     public String mappingGetV2() {
         log.info("mappingGetV2");
         return "OK!";   // 문자열을 HTTP Message Body에 그대로 넣는다.
+    }
+
+    /**
+     * PathVariable 사용: 변수명이 같으면 생략할 수 있다.
+     * @PathVariable("userId") String userId => @PathVariable userId
+     * 요청 URL 자체에 값이 들어간 경우(userId), 이를 경로 변수라고 칭한다.
+     * 메서드의 매개변수로 경로 변수를 받을 때, @PathVariable을 사용할 수 있다.
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        // 만약 매개변수 이름이 userId로 경로 변수와 동일할 시,
+        // (@PathVariable String userId)로 변경 가능하다.
+        log.info("mappingPath userId={}", data);
+        return "OK!";
     }
 }
