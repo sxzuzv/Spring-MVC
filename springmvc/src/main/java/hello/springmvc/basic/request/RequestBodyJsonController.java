3a0b3805d677c112cdf4032182053e1605db1b15
soojin <pofoxz@gmail.com>
2024-05-01 18:27:42 +0900

    Add: HTTP 요청 메시지 JSON - V1

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,42 @@
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {"username":"hello", "age":20}
+ * content-type: application/json
+ */
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+    // JSON 결과를 파싱해 Java 객체로 변환하려면 JSON 변환 라이브러리(jackson, Gson 등)를 추가해서 사용해야 한다.
+    // SpringBoot로 Spring MVC를 선택하면 기본으로 jackson 라이브러리(ObjectMapper)를 함께 제공한다.
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        // jackson 라이브러리를 이용하여 JSON을 객체(HelloData) 형태로 변경하고, 값을 읽는다.
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        response.getWriter().write("@k!");
+    }
+}
