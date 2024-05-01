a7c167247dcd322ed3c168019e6b350567e03140
soojin <pofoxz@gmail.com>
2024-05-02 00:55:57 +0900

    Add: HTTP 응답(HTTP API, Message Body에 직접 입력) - 바인딩 객체 형태로 반환 및 @ResponseStatus 사용

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
 
@@ -40,5 +41,20 @@ public class ResponseBodyController {
         helloData.setAge(20);
 
         return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        // v2 방식의 경우, 반환 형태를 HelloData 객체로 설정해 코드 간소화를 진행하였다.
+        // 그러나 v1 방식과는 달리 HTTP 상태 코드를 지정할 수가 없다.
+        // 이를 해결하기 위해 @ResponseStatus 애너테이션이 제공되며, 이를 통해 HTTP 상태 코드 설정이 가능하다.
+        HelloData helloData = new HelloData();
+
+        helloData.setUsername("soo");
+        helloData.setAge(20);
+
+        return helloData;
     }
 }
