cff395a557d33cd19aba9a4284dfa98bbce558fd
soojin <pofoxz@gmail.com>
2024-05-01 18:40:45 +0900

    Comment: 주석 추가

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -58,13 +58,16 @@ public class RequestBodyJsonController {
         return "@k!";
     }
 
+    /**
+     * @RequestBody에 직접 만든 객체(HelloData)를 지정할 수 있다.
+     * 이때, @RequestBody는 생략 불가하다. -> @ModelAttribute가 적용되기 때문!
+     * HttpMessageConverter를 사용하여 V2의 objectMapper 활용 과정을 자동화
+     * 요청 메시지가 JSON일 경우, MappingJackson2HttpMessageConverter 사용 (content-type:application/json)
+     */
     @ResponseBody
     @PostMapping("/request-body-json-v3")
-    public String requestBodyJsonV3(@RequestBody HelloData helloData)
-            throws IOException {
-        // @RequestBody에 직접 만든 객체(HelloData)를 지정할 수 있다.
+    public String requestBodyJsonV3(@RequestBody HelloData helloData) {
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
-
         return "@k!";
     }
 }
