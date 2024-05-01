321f5cc224fb69aa69bd2bf0cd2d6a9204b1086c
soojin <pofoxz@gmail.com>
2024-05-01 17:47:51 +0900

    Add: HTTP 요청 메시지 단순 텍스트 - HttpEntity 적용

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,29 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
 
         responseWriter.write("OK!");
+    }
+
+    /**
+     * HttpEntity: HTTP Header, Body 정보를 편리하게 조회할 수 있다.
+     * - 메시지 바디 정보를 직접 조회 (@RequestParam, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 응답 시에도 HttpEntity를 사용할 수 있다.
+     * - 메시지 바디의 정보를 직접 반환한다. (view 조회 X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity)
+            throws IOException {
+        // HttpEntity 이용 시, 아래의 코드를 자동으로 수행해준다. (지정한 String 형태로 자동 변환)
+        // String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        // httpEntity.getBody(): 변환된 Http Message Body의 내용을 가져온다.
+        String messageBody = httpEntity.getBody();
+
+        log.info("messageBody={}", messageBody);
+
+        // 반환 메시지도 HttpEntity를 활용해 작성할 수 있다.
+        return new HttpEntity<>("OK!");
     }
 }
