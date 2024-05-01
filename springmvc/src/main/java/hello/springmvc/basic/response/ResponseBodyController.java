39ddee94c08842f2cdc23957b841ce2a11009574
soojin <pofoxz@gmail.com>
2024-05-02 00:46:40 +0900

    Add: HTTP 응답(HTTP API, Message Body에 직접 입력) - ResponseEntity 사용

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -2,6 +2,9 @@ package hello.springmvc.basic.response;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,5 +16,10 @@ public class ResponseBodyController {
     @GetMapping("/response-body-string-v1")
     public void responseBodyV1(HttpServletResponse response) throws IOException {
         response.getWriter().write("@K!");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() throws IOException {
+        return new ResponseEntity<>("@K!", HttpStatus.OK);
     }
 }
